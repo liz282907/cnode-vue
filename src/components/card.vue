2fd2937708706eca2a3844572f989b2e4a9902c7
liz282907 <422364804@qq.com>
2016-11-12 23:40:41 +0800

    增加card组件

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="">
-    <div class="left">
+    <h3>
       <span :class="tagClassName"></span>
+      {{title}}
+    </h3>
+    <div class="info">
+      <img class="avatar" :src="imgSrc" alt="avatar">
+      <div class="status">
+         <p>
+           <span class="creator">{{creator}}</span>
+           <span class="stats">
+              <em>{{clickCount}}/</em>
+              {{commentCount}}
+            </span>
+         </p>
+         <p class="">
+           <time>{{createTime}}</time>
+           <time>{{updateTime}}</time>
+         </p>
+      </div>
     </div>
-    <div class="right">
 
-    </div>
   </div>
 </template>
 
@@ -13,18 +28,37 @@
 export default {
   name: 'card',
   props:{
+    isTop: {
+      type: Boolean,
+      default: false
+    },
+    title:"",
     customStyle:{
       type: Object,
       default:()=>{} //type为object的需要在function中返回
-    }
-  },
-  computed:{
-    tagClassName:['tag',{'tag-highlight':isTop}]
+    },
+    imgSrc: "",
+    commentCount: {
+      type: Number,
+      default: 0
+    },
+    clickCount: {
+      type: Number,
+      default: 0
+    },
+    creator: "",
+    createTime: "",
+    updateTime: "",
+
   },
   data() {
     return {
-      isTop: false
-    };
+    }
+  },
+  computed:{
+    tagClassName(){
+      return ['tag',{'tag-highlight':this.isTop}]
+    },
   },
 };
 </script>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,11 +1,38 @@
 <template>
-  <nav-head></nav-head>
+  <div>
+      <nav-head></nav-head>
+      <ul>
+        <li v-for="item in postList">
+          <card :title="item.title" :isTop="item.isTop" :imgSrc="item.imgSrc"
+                :commentCount.number="item.commentCount"
+                :clickCount.number="item.clickCount"
+                :creator="item.creator"
+                :createTime="item.createTime"
+                :updateTime="item.updateTime"
+
+          ></card>
+        </li>
+      </ul>
+  </div>
 
 </template>
-
 <script>
 import NavHead from '../components/header';
 import Card from '../components/card';
+
+const postList = [
+  {
+    // customStyle:,
+    title: "光棍节程序员闯关",
+    // imgSrc: "./assets/images/logo.png",
+    commentCount: 3552,
+    clickCount: 26,
+    creator: "Hanggi",
+    createTime: "2016-09-27 15:53:31",
+    updateTime: "1天前",
+    isTop: false
+  }
+]
 
 export default {
   name: 'app',
@@ -13,6 +40,15 @@ export default {
     'nav-head': NavHead,
     'card': Card
   },
+  created(){
+
+  },
+  data(){
+    return {
+      postList: postList
+    }
+  }
+
 };
 </script>
 
