5a53fbfa839992954546fbb9552addfa976d323f
juyl <juyl@broada.com>
2016-11-14 23:25:45 +0800

    slide vue animation

--- src/components/slide-card.vue
+++ src/components/slide-card.vue
@@ -1,6 +1,6 @@
 <template>
     <transition name="slide-fade">
-        <div v-if="show" class="left-slide-card">
+        <div v-show="show" class="left-slide-card">
             <div class="layer-shade" @click="cancel"></div>
             <section class="left-nav-bar">
                 <div class="user-login-wrapper">
@@ -59,24 +59,30 @@ export default {
 
 </script>
 <style lang="scss">
-    .slide-fade-enter-active, .slide-fade-leave-active {
+    .slide-fade-enter-active.left-slide-card, .slide-fade-leave-active.left-slide-card {
         transition: all 0.5s ease;
-            width: 200;
+        .left-nav-bar {
+            transition: all 0.5s ease;
+            width: 200px;
+        }
         
     }
-    .slide-fade-enter, .slide-fade-leave-active {
+    .slide-fade-enter.left-slide-card, .slide-fade-leave-active.left-slide-card {
+        .left-nav-bar {
             width: 0;
+        }
     }
     .left-slide-card{
+       
+    }
+    .left-nav-bar {
         position: fixed;
         top: 0;
         left: 0;
-        bottom: 0;
         width: 200px;
-        padding-left: 30px;
-   
-        background: #fff; 
-        border-right: 1px solid #b3b3b3;
+        bottom: 0;
+        background: #fff;
+        z-index: 10000;
     }
     .layer-shade {
         position: fixed;
@@ -88,7 +94,5 @@ export default {
         background-color: #000;
         opacity: 0.3;
     }
-    .left-nav-bar {
-        z-index: 10000;
-    }
+    
 </style>
