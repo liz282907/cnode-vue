b05aa35e68e19529abc73aea70bd8ccd42ce8c5a
juyl <juyl@broada.com>
2016-11-15 22:54:00 +0800

    slide css

--- src/components/slide-card.vue
+++ src/components/slide-card.vue
@@ -6,10 +6,38 @@
                 <div class="user-login-wrapper">
                     <div v-if="isLogin">
                     </div>
-                    <div v-else>
+                    <div v-else class="not-login">
                         <i class="iconfont icon-login"></i>
+                        <span>登入</span>
                     </div>
                 </div>
+                <div class="split-line"></div>
+                <ul class="link-list">
+                    <li>
+                        <i class="iconfont icon-login"></i>
+                        <span>全部</span>
+                    </li>
+                    <li>
+                        <i class="iconfont icon-login"></i>
+                        <span>精华</span>
+                    </li>
+                    <li>
+                        <i class="iconfont icon-login"></i>
+                        <span>分享</span>
+                    </li>
+                    <li class="border-bottom">
+                        <i class="iconfont icon-login"></i>
+                        <span>招聘</span>
+                    </li>
+                    <li>
+                        <i class="iconfont icon-login"></i>
+                        <span>消息</span>
+                    </li>
+                    <li>
+                        <i class="iconfont icon-login"></i>
+                        <span>关于</span>
+                    </li>
+                </ul>
             </section>
         </div>>
     </transition>
@@ -40,19 +68,11 @@ export default {
         }
     },
     beforeUpdate() {
-        console.log(this)
-        // if(this.modalShow != this.show) {
-        //     this.modalShow = this.show;
-        // }
-         
     },
     methods: {
     },
     watch: {
-        // show(val) {
-        //     console.log(val)
-        //     this.modalShow = val;
-        // }
+       
     }
 
 }
@@ -63,13 +83,15 @@ export default {
         transition: all 0.5s ease;
         .left-nav-bar {
             transition: all 0.5s ease;
-            width: 200px;
+            left: 0;
         }
         
     }
     .slide-fade-enter.left-slide-card, .slide-fade-leave-active.left-slide-card {
         .left-nav-bar {
-            width: 0;
+            left: -200px;
+            padding-left: 0;
+            padding-right: 0;
         }
     }
     .left-slide-card{
@@ -81,8 +103,33 @@ export default {
         left: 0;
         width: 200px;
         bottom: 0;
+        padding: 0 30px;
         background: #fff;
+        overflow: hidden;
         z-index: 10000;
+       
+    }
+    .user-login-wrapper {
+        padding: 30px;
+        padding-left: 0;
+        line-height: 30px;
+        border-bottom: 0.5px solid #d4d4d4; 
+        span {
+            margin-left: 10px;
+        }
+        .icon-login {
+            font-size: 30px;
+            vertical-align: middle;
+        }
+
+    }
+    .link-list {
+        li {
+            line-height: 40px;
+        }
+        .border-bottom {
+            border-bottom: 1px solid #d4d4d4;
+        }
     }
     .layer-shade {
         position: fixed;
